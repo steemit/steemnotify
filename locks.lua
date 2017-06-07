66a44475b98a01a9a7149ad1625bd1fa08c5ebab
valzav <i@valzav.com>
2017-06-07 17:50:01 -0400

    use 30 seconds timeout in lock_entity call

--- locks.lua
+++ locks.lua
@@ -1,11 +1,18 @@
 function lock_entity(entity_name)
     local space = box.space.locks
     local res = space:select{entity_name}
+    local current_time = math.floor(fiber.time())
     if #res > 0 then
       local tuple = res[1]
-      return false
+      local lock_time = tuple[2]
+      if current_time - lock_time < 30 then
+        return false
+      else
+        space:update(entity_name, {{'=', 2, current_time}})
+      end
+      return true
     else
-      local tuple = {entity_name}
+      local tuple = {entity_name, current_time}
       space:insert(tuple)
       return true
     end
